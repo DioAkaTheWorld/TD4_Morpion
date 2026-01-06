d413ad5d984ac50ceebeab4c59d1567cf4cb41be
Baptiste <delabordebaptiste8@gmail.com>
2026-01-06 16:32:16 +0100

    exo3

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TD4_Morpion
+
+gYO4fay0HuYa
--- TD4_Morpion/src/api/index.js
+++ TD4_Morpion/src/api/index.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://morpion-api.edu.netlor.fr',
-    headers: {
-              'Content-Type': 'application/json',
-              'Authorization': 'gYO4fay0HuYa'
-    }
+  baseURL: 'https://morpion-api.edu.netlor.fr',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `key=${import.meta.env.VITE_API_KEY}`
+  }
 })
